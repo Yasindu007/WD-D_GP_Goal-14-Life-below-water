6d181861412718aad1c55d446e9c30e9a3ca66bd
jlinuke <fernandolinuke@gmail.com>
2025-03-10 10:30:56 +0530

    teamfinal

--- JS/feedback.js
+++ JS/feedback.js
@@ -1,37 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const nameinput = document.getElementById("nameinput");
-    const email = document.getElementById("email");
-    const suggest = document.getElementById("Suggest");
-    const formpreview = document.getElementById('formpreview');
-    const addition = document.getElementById('additionq');
-
-    const previewName = document.getElementById("previewName");
-    const previewEmail = document.getElementById("previewEmail");
-    const previewVisit = document.getElementById("previewVisit");
-    const previewInformative = document.getElementById("previewInformative");
-    const previewSuggestion = document.getElementById("previewSuggestion");
-    const previewSatisfaction = document.getElementById("previewSatisfaction");
-    const previewRecommendation = document.getElementById("previewRecommendation");
-    const previewAdditional = document.getElementById("previewAdditional");
-
-    function updatePreview() {
-        const visit = document.querySelector("input[name='visiting']:checked");
-        const informative = document.querySelector("input[name='informative']:checked");
-        const ratingscale = document.querySelector('input[name="satisfaction"]:checked');
-        const recomend = document.querySelector('input[name="recommendation"]:checked');
-
-        previewName.textContent = `Name: ${nameinput ?nameinput.value:"empty"}`;
-        previewEmail.textContent = `Email: ${email ?email.value:"empty"}`;
-        previewVisit.textContent = `Already Visited: ${visit ? visit.value : "Not Selected"}`;
-        previewInformative.textContent = `Is the website informative and Easy to navigate: ${informative ? informative.value : "Not Selected"}`;
-        previewSuggestion.textContent = `Suggestions: ${suggest? suggest.value:"empty"}`;
-        previewSatisfaction.textContent = `Satisfaction Score: ${ratingscale ? ratingscale.value : "Not Selected"}`;
-        previewRecommendation.textContent = `Do you Recommend Us?: ${recomend ? recomend.value : "Not Selected"}`;
-        previewAdditional.textContent = `Questions & Requests: ${addition ? addition.value:"empty"}`;
-    }
-
-    formpreview.addEventListener('click', updatePreview);
-
 
 
     function checkradioinputs() {
